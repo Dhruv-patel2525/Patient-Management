e2a96ee366eb73bc5b72c5e576be5ba6c54c9696
Dhruvipatel2525 <www.dp454@gmail.com>
2025-08-27 12:58:14 -0300

    create kafka Producer for async response and publish to kafka Broker.

--- patient-service/src/main/java/com/pm/patientservice/service/PatientService.java
+++ patient-service/src/main/java/com/pm/patientservice/service/PatientService.java
@@ -5,6 +5,7 @@ import com.pm.patientservice.dto.PatientResponseDTO;
 import com.pm.patientservice.exception.EmailAlreayExistsException;
 import com.pm.patientservice.exception.PatientNotFoundException;
 import com.pm.patientservice.grpc.BillingServiceGrpcClient;
+import com.pm.patientservice.kafka.KafkaProducer;
 import com.pm.patientservice.mapper.PatientMapper;
 import com.pm.patientservice.models.Patient;
 import com.pm.patientservice.repository.PatientRepository;
@@ -21,8 +22,12 @@ import java.util.stream.Collectors;
 public class PatientService {
     private final PatientRepository patientRepository;
     private final BillingServiceGrpcClient billingServiceGrpcClient;
-    public PatientService(PatientRepository patientRepository,BillingServiceGrpcClient billingServiceGrpcClient)
+    private final KafkaProducer kafkaProducer;
+    public PatientService(PatientRepository patientRepository,
+                          BillingServiceGrpcClient billingServiceGrpcClient,
+                          KafkaProducer kafkaProducer)
     {
+        this.kafkaProducer=kafkaProducer;
         this.patientRepository=patientRepository;
         this.billingServiceGrpcClient= billingServiceGrpcClient;
     }
@@ -41,6 +46,7 @@ public class PatientService {
         Patient newPatient= patientRepository.save(PatientMapper.toModel(patientRequestDTO));
         System.out.println("Saved data successfully");
         billingServiceGrpcClient.createBillingAccount(newPatient.getId().toString(),newPatient.getName(),newPatient.getEmail());
+        kafkaProducer.sendEvent(newPatient);
         return PatientMapper.toDTO(newPatient);
     }
 
